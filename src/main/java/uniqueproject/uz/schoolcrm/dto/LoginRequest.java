7de6cc1879963360717b2249ab3340dfa6fee30a
JahongirDevelop <sjahongir221@gmail.com>
2024-07-08 17:10:54 +0500

    changing are some classes

--- src/main/java/uniqueproject/uz/schoolcrm/dto/LoginRequest.java
+++ src/main/java/uniqueproject/uz/schoolcrm/dto/LoginRequest.java
@@ -1,4 +1,12 @@
 package uniqueproject.uz.schoolcrm.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class LoginRequest {
     private String phone;
     private String password;
