151335e0b295a8bea5389f4676eab146dcac48a0
JahongirDevelop <sjahongir221@gmail.com>
2024-07-09 15:19:17 +0500

    changes in entities

--- src/main/java/uniqueproject/uz/schoolcrm/entity/BaseEntity.java
+++ src/main/java/uniqueproject/uz/schoolcrm/entity/BaseEntity.java
@@ -14,7 +14,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
 
