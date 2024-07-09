a10d3fb4894402642f595646902e4523747b749a
JahongirDevelop <sjahongir221@gmail.com>
2024-07-09 15:16:20 +0500

    changes in entities

--- src/main/java/uniqueproject/uz/schoolcrm/entity/UserEntity.java
+++ src/main/java/uniqueproject/uz/schoolcrm/entity/UserEntity.java
@@ -1,11 +1,7 @@
 package uniqueproject.uz.schoolcrm.entity;
-
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.web.servlet.handler.UserRoleAuthorizationInterceptor;
 import uniqueproject.uz.schoolcrm.entity.enums.UserRole;
-
-import java.util.UUID;
 
 @Entity
 @AllArgsConstructor
@@ -17,7 +13,6 @@ public class UserEntity extends BaseEntity {
     private String phone;
     private String password;
     private UserRole role; // ADMIN, INSTRUCTOR, STUDENT
-
 
 }
 
