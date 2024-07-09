57d3fe576f60e5695c6b343d7b2c8db7a1db1393
JahongirDevelop <sjahongir221@gmail.com>
2024-07-09 15:17:15 +0500

    changes in entities

--- src/main/java/uniqueproject/uz/schoolcrm/entity/Student.java
+++ src/main/java/uniqueproject/uz/schoolcrm/entity/Student.java
@@ -1,11 +1,8 @@
 package uniqueproject.uz.schoolcrm.entity;
-
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 @Entity
 @AllArgsConstructor
